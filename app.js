b1f82c01ef708cecbaf978e68296abbaad93aa4c
Thor40 <nicholas.kosik@gmail.com>
2020-05-12 13:10:03 -0500

    add multiple projects

--- app.js
+++ app.js
@@ -34,7 +34,10 @@ const promptUser = () => {
   ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+  if (!portfolioData.projects) {
+  portfolioData.projects = [];
+  }
   console.log(`
 =================
 Add a New Project
@@ -74,10 +77,18 @@ Add a New Project
       message: 'Would you like to enter another project?',
       default: false
     }
-  ]);
+  ]).then(projectData => {
+    portfolioData.projects.push(projectData);
+    if(projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
