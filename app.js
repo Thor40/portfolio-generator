29b7b35f2c6c8bc787ddfb949dd8772d751095f0
Thor40 <nicholas.kosik@gmail.com>
2020-05-13 14:43:45 -0500

    template

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const profileDataArgs = process.argv.slice(2);
 
@@ -137,5 +137,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
